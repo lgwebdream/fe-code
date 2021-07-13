d7cdc39edaf1294327e6749ace5709f357db4480
水逆 <>
2021-07-13 23:53:39 +0800

    feat: react template environment and demo init

---
+++ examples/react-demo/src/pages/views/other.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Other() {
+  return <div>其他</div>;
+}
---
+++ lib/react/components/Input.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+/**
+ * 输入框
+ *
+ * @param param
+ * @returns
+ */
+export default function FInput({}) {
+  return <div></div>;
+}
