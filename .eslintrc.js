6b7a0100028de00d0f370a8401f46b477201cb0e
水逆 <huyi247767221@163.com>
2021-07-14 18:19:18 +0800

    feat: define CRUD config interface

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,19 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  extends: ['airbnb-base', 'airbnb-typescript', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 12,
+    project: './tsconfig.eslint.json',
   },
   rules: {
     'no-console': 'off',
     'no-restricted-syntax': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
+  ignorePatterns: [
+    '.eslintrc.js',
+    'iwr.config.js',
+    'examples/react-demo/api/index.js',
+  ],
 };
