5bf0c62f2acc07fc91347535b668ac36f1a8f744
zqzhan@kaikeba.com <zqzhan@kaikeba.com>
2021-07-22 10:55:53 +0800

    feat: lib vue init

---
+++ lib/vue/examples/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Demo1 />
+</template>
+
+<script>
+import Demo1 from './components/Demo1.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Demo1,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
