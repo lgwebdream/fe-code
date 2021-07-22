425bdccbe11c65cf49b93a58e0cd7d27e7ed7e3a
水逆 <huyi247767221@163.com>
2021-07-22 22:32:42 +0800

    feat: complete row's operation of table

--- lib/react/components/Crud.tsx
+++ lib/react/components/Crud.tsx
@@ -3,10 +3,15 @@ import Table from './Table';
 import ToolBar from './ToolBar';
 import Form from './Form';
 import { ICrud, ICrudColumn } from './Crud.d';
+import { BatchButtonGroup } from './ToolBar/BatchOperation';
 
 const FCrud = (props: ICrud): React.ReactElement => {
-  const { columns, tableProps, batchToolbar, request } = props;
+  const { columns, tableProps, batchToolbar, rowToolbar, request } = props;
   const [filter, setFilter] = useState(null);
+  const [selection, setSelection] = useState({
+    selectedRows: [],
+    selectedRowKeys: [],
+  });
 
   const onSearchFilter = (params: {}) => setFilter(params);
   const onResetFilter = () => setFilter(null);
@@ -25,9 +30,39 @@ const FCrud = (props: ICrud): React.ReactElement => {
     return buffer;
   }, [columns]);
 
+  // 表格自动处理
+  const tableColumns = useMemo(() => {
+    const buffer: ICrudColumn[] = [];
+
+    columns.forEach(it => {
+      const temp = { ...it };
+      buffer.push(temp);
+    });
+
+    if (rowToolbar) {
+      buffer.push({
+        title: '操作',
+        width: 200,
+        dataIndex: 'operation',
+        render: (text, row, index: number) => {
+          return (
+            <BatchButtonGroup
+              options={rowToolbar}
+              args={{ row, rowKey: index }}
+            />
+          );
+        },
+      } as ICrudColumn);
+    }
+
+    return buffer;
+  }, [columns, rowToolbar]);
+
   return (
     <div className="f-crud">
       <ToolBar
+        selectedRows={selection.selectedRows}
+        selectedRowKeys={selection.selectedRowKeys}
         batchOptions={batchToolbar}
         searchOptions={{
           columns: filterColumns,
@@ -36,9 +71,17 @@ const FCrud = (props: ICrud): React.ReactElement => {
         }}
       />
       <Table
+        rowSelection={{
+          columnWidth: 60,
+          type: 'checkbox',
+          onChange: (
+            selectedRowKeys: React.Key[],
+            selectedRows: Record<string, any>[],
+          ) => setSelection({ selectedRows, selectedRowKeys }),
+        }}
         {...tableProps}
         request={request}
-        columns={columns}
+        columns={tableColumns}
         params={filter}
       />
     </div>
