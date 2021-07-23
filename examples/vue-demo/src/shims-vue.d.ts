4c96dcccd593573a8addb69f28719707e408b500
zqzhan@kaikeba.com <zqzhan@kaikeba.com>
2021-07-23 17:59:30 +0800

    fix: 修复vue router版本问题

--- examples/vue-demo/src/shims-vue.d.ts
+++ examples/vue-demo/src/shims-vue.d.ts
@@ -1,24 +1,24 @@
-// // declare module '*.vue' {
-// //   import { ComponentOptions } from 'vue';
-// //   const componentOptions: ComponentOptions;
-// //   export default componentOptions;
-// // }
+declare module '*.vue' {
+  import { ComponentOptions } from 'vue';
+  const componentOptions: ComponentOptions;
+  export default componentOptions;
+}
 
-// declare module 'vue/types/vue' {
-//   import VueRouter, { Route } from 'vue-router';
-//   interface Vue {
-//     $router: VueRouter; // 这表示this下有这个东西
-//     $route: Route;
-//     $http: any;
-//     $Message: any;
-//     $Modal: any;
-//   }
-// }
-// declare interface Window {
-//   Vue: any;
-// }
+declare module 'vue/types/vue' {
+  import VueRouter, { Route } from 'vue-router';
+  interface Vue {
+    $router: VueRouter; // 这表示this下有这个东西
+    $route: Route;
+    $http: any;
+    $Message: any;
+    $Modal: any;
+  }
+}
+declare interface Window {
+  Vue: any;
+}
 
-// declare module '*.css';
-// declare module '*.less';
-// declare module '*.png';
-// declare module '*.svg';
+declare module '*.css';
+declare module '*.less';
+declare module '*.png';
+declare module '*.svg';
