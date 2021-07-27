6e16616b798077a48e557e7c36bf64f120092b52
Xianweiwu <xianweics@163.com>
2021-07-27 13:43:52 +0800

    feat: add dependencies.config

---
+++ dependencies.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dependencies: {
+    react: '^17.2.0',
+    vue: '^2.6.14',
+    '@morgul/snowpack-plugin-vue2': '^0.4.2',
+  },
+  devDependencies: {
+    snowpack: '^3.8.2',
+  },
+};
