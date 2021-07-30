582ff754331e8b16d9457047e0e80669ee5a55b1
zqzhan@kaikeba.com <zqzhan@kaikeba.com>
2021-07-31 00:07:14 +0800

    feat: table crud处理

--- examples/vue-demo/src/views/TableDemo.vue
+++ examples/vue-demo/src/views/TableDemo.vue
@@ -1,54 +1,34 @@
 <template>
   <div>
-    <v-table :tableData='tableData' :column='column' sequence :total='total'></v-table>
+    <v-table :request='request' :columns='columns' :title='title'></v-table>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import axios from 'axios';
+import { ICrud } from '@fe-code/vue/components/CrudTypes';
 
 export default defineComponent({
   name: 'Demo1',
   props: {},
   data() {
+    const demoTable: ICrud = {
+      title: '人员管理',
+      columns: [
+        { dataIndex: 'date', title: '日期', readonly: true },
+        { dataIndex: 'name', title: '姓名' },
+        { dataIndex: 'address', title: '地址' },
+      ],
+      request: async params => {
+        return axios.get('/api/json/list', { params });
+      },
+    };
     return {
-      tableData: [
-        {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄',
-        },
-        {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄',
-        },
-        {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄',
-        },
-        {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄',
-        },
-      ],
-      column: [
-        { prop: 'date', label: '日期', width: '180' },
-        { prop: 'name', label: '姓名', width: '180' },
-        { prop: 'address', label: '地址' },
-      ],
-      total: 0,
+      ...demoTable,
     };
   },
-  async mounted() {
-    const res = await axios.get('/api/json/list');
-    const { data, total } = res?.data;
-    this.tableData = data;
-    this.total = total || 0;
-  },
+  // async mounted() {},
 });
 </script>
 
