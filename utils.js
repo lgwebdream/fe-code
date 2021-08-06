a488bcb3fe9b4d9eb951ed96f371ce5572756df4
Xianweiwu <xianweics@163.com>
2021-08-06 17:47:13 +0800

    feat: add notes for call patchPackageJson method

--- utils.js
+++ utils.js
@@ -1,9 +1,11 @@
 const { writeJsonSync } = require('fs-extra');
 const { componentDependencies } = require('./dependencies.config');
+
 /**
  * patch dependencies to package.json
- * @param packagesPath: package path
- * @param main: react/vue
+ * @param packagesPath{string} package path
+ * @param main{string} react/vue
+ * @return {void}
  */
 module.exports.patchPackageJson = ({ packagesPath, main }) => {
   const packages = require(packagesPath);
